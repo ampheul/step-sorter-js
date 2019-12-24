b19ca8e7a157da1cd671149f30457c737a4c27c4
Thomas Vandeven <tvandev4@uwo.ca>
2019-12-24 05:37:31 -0500

    applied js-beautify applied js-beautify to step-sorter.js and updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# step-sorter.js
-A javascript port of elm-step-sorter
+# Step Sorter Js
+A javascript rewrite of [elm-step-sorter](https://github.com/ampheul/elm-step-sorter).
+
+## Use
+This module is designed for sorting an array step by step, based on manual choices by the user. Check out the example file for how to call this code in your project.
+
+## Demo
+A simple demo web app is available to demonstrate what is possible [here](https://ampheul.github.io/elm-step-sorter/). It is programmed in elm, but the concepts are almost identical in javascript.
+
+*Note:* This app works on local upload only. Source code for the demo is available on [elm-step-sorter](https://github.com/ampheul/elm-step-sorter).
+
+## Motivation
+
+The inspiration behind this code came from some discussions with a close friend. We noticed that it is difficult to be objective when rating things. For example, you might rate a good movie as average on a day when you're feeling critical, and an average movie as good the next day. The scores would become inconsistent and they wouldn't really tell you what your favorite movies are. Therefore a better way to rate things could be to rank them relative to one another. We produce this ranking by sorting your list of objects. Comparisons cost users time. In order to minimize cost we use Merge sort, which requires the fewest number of comparisons asymptotically (and practically).
--- step-sorter.js
+++ step-sorter.js
@@ -11,7 +11,7 @@ var typed_obj = (the_type, data) => {
 
     return val;
 }
-   
+
 
 
 //-- UTILITY
@@ -20,11 +20,11 @@ var eq = 0;
 var gt = 1;
 var lt = 2;
 
-var cons_nonempty = ([a,as], b)=>[b, [a,...as]];
+var cons_nonempty = ([a, as], b) => [b, [a, ...as]];
 
 var choices = (sorting) => {
-    let [,,merge] = sorting[data_key];
-    let [,[l], [r]] = merge[data_key];
+    let [, , merge] = sorting[data_key];
+    let [, [l],[r]] = merge[data_key];
     return [l, r];
 };
 
@@ -35,27 +35,23 @@ var merging = 'merging';
 
 // left and right are type (T, T[])
 var make_merging =
-    (stack, left, right) => typed_obj(merging, [stack,left,right]);
+    (stack, left, right) => typed_obj(merging, [stack, left, right]);
 
 var merge_step = (ord, merge) => {
-    let [stack, [l,ls], [r,rs]] = merge[data_key];
+    let [stack, [l, ls], [r, rs]] = merge[data_key];
 
     if (ord === eq) {
         return merge_step(gt, merge);
-    }
-    else if (ord === gt && ls.length > 0) {
-        let [x,...xs] = ls;
-        return make_merging([l,...stack], [x,xs], [r,rs]);
-    }
-    else if (ord === gt && ls.length === 0) {
-        return stack.reduce(cons_nonempty, [l,[r,...rs]]);
-    }
-    else if (ord === lt && rs.length > 0) {
-	let [x,...xs] = rs;
-        return make_merging([r,...stack], [x,xs], [l,ls]);
-    }
-    else if (ord === lt && rs.length === 0) {
-	return stack.reduce(cons_nonempty, [r, [l,...ls]]);
+    } else if (ord === gt && ls.length > 0) {
+        let [x, ...xs] = ls;
+        return make_merging([l, ...stack], [x, xs], [r, rs]);
+    } else if (ord === gt && ls.length === 0) {
+        return stack.reduce(cons_nonempty, [l, [r, ...rs]]);
+    } else if (ord === lt && rs.length > 0) {
+        let [x, ...xs] = rs;
+        return make_merging([r, ...stack], [x, xs], [l, ls]);
+    } else if (ord === lt && rs.length === 0) {
+        return stack.reduce(cons_nonempty, [r, [l, ...ls]]);
     }
 };
 
@@ -66,45 +62,42 @@ var sorting = 'sorting';
 
 // note that merged, unmerged are of type (T,T[])[], merge is a merging
 var make_sorting =
-    (merged, unmerged, merge) => typed_obj(sorting, [merged, unmerged,merge]);
+    (merged, unmerged, merge) => typed_obj(sorting, [merged, unmerged, merge]);
 
 var init_sort = (xs) => {
     if (xs.length > 1) {
-	let [x,y,...ys] = xs;
-	return  make_sorting([], ys.map((a)=>[a,[]]), make_merging([], [x,[]], [y,[]]));
-    }
-    else {
-	return xs;
+        let [x, y, ...ys] = xs;
+        return make_sorting([], ys.map((a) => [a, []]), make_merging([], [x, []], [y, []]));
+    } else {
+        return xs;
     }
 };
 
 var sort_step = (ord, sort) => {
     [merged, unmerged, merge] = sort[data_key];
-    
+
     // step forward in the merge, only place we use ord
     merge = merge_step(ord, merge);
 
     // if the merge is still ongoing, return an updated sort
-    if (typeof(merge)==='object' && merge[type_key] === merging) {
+    if (typeof(merge) === 'object' && merge[type_key] === merging) {
         return make_sorting(merged, unmerged, merge);
     }
     // pick two more lists to merge, the continue as before
     else if (unmerged.length > 1) {
-        let [x,y,...ys] = unmerged;
-	return make_sorting([merge,...merged], ys, make_merging([], x, y));
-    }
-    else if (unmerged.length == 1) {
-	let [x] = unmerged;
-	return make_sorting(merged, [], make_merging([], x, merge));
-    }
-    else if (unmerged.length == 0 && merged.length > 0) {
-        let [x,...xs] = merged;
-	return make_sorting([], xs, make_merging([],merge,x));
+        let [x, y, ...ys] = unmerged;
+        return make_sorting([merge, ...merged], ys, make_merging([], x, y));
+    } else if (unmerged.length == 1) {
+        let [x] = unmerged;
+        return make_sorting(merged, [], make_merging([], x, merge));
+    } else if (unmerged.length == 0 && merged.length > 0) {
+        let [x, ...xs] = merged;
+        return make_sorting([], xs, make_merging([], merge, x));
     }
     // otherwise all the sorting is done
     else if (unmerged.length == 0 && merged.length == 0) {
-	let [x,xs] = merge
-	return [x,...xs];
+        let [x, xs] = merge
+        return [x, ...xs];
     }
 };
 
@@ -112,12 +105,12 @@ var sort_step = (ord, sort) => {
 //-- EXPORTS
 
 module.exports = {
-    eq : eq,
-    gt : gt,
-    lt : lt,
-    init_sort : init_sort,
-    sort_step : sort_step,
-    type : type_key,
-    data : data_key,
-    choices : choices
+    eq: eq,
+    gt: gt,
+    lt: lt,
+    init_sort: init_sort,
+    sort_step: sort_step,
+    type: type_key,
+    data: data_key,
+    choices: choices
 };
